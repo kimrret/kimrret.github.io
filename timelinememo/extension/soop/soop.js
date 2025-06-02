7e4872631757448cd3085fa267a269d7c5916f7c
kimrret <72913333+kimrret@users.noreply.github.com>
2025-06-03 01:56:57 +0900

    Update soop.js

--- timelinememo/extension/soop/soop.js
+++ timelinememo/extension/soop/soop.js
@@ -1,4 +1,4 @@
-console.log("location.hostname", location.hostname);
+console.log("location.hostnameaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa");
 const isLive = location.hostname.startsWith("play");
 const isVOD = location.hostname.startsWith("vod");
 
